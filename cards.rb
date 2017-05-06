d405d2c8a6ace33f4be2c5b44fc67b9b4b0ce2be
Paul Chicarello <paul@chicarello.com>
2017-05-06 12:09:39 -0400

    General functionality flushed out

--- cards.rb
+++ cards.rb
@@ -1,12 +1,28 @@
-require 'net/http'
-require 'json'
 require_relative 'functions'
 
 prompt = '> '
+
+hand = Hash.new(0)
+
+show_hand(hand)
 
 puts 'Welcome to my card game.'
 puts prompt
 
 id = new_game
 card = draw_card(id)
-puts card
+hand[card] += 1
+
+card = draw_card(id)
+hand[card] += 1
+
+card = draw_card(id)
+hand[card] += 1
+
+card = draw_card(id)
+hand[card] += 1
+
+card = draw_card(id)
+hand[card] += 1
+
+show_hand(hand)
--- functions.rb
+++ functions.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'json'
+
 def new_game()
   uri = URI('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
   my_hash = JSON.parse(Net::HTTP.get(uri))
@@ -7,5 +10,14 @@ end
 def draw_card(deck_id)
   uri = URI("https://deckofcardsapi.com/api/deck/#{deck_id}/draw/?count=1")
   my_hash = JSON.parse(Net::HTTP.get(uri))
-  puts my_hash['cards'].first['value']
+  return my_hash['cards'].first['value']
+end
+
+def show_hand(hand)
+  if hand.empty?
+    puts 'You have no cards in your hand!'
+    return
+  end
+  puts 'Cards in your hand:'
+  hand.each { |card, count| puts "There are #{count} #{card}(s)" }
 end
