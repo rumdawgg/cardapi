24cd922b1eedebcc141d2a6b49797bd3a445dbe4
Paul Chicarello <paul@chicarello.com>
2017-05-07 21:31:05 -0400

    Got a decent working version now

--- cards.rb
+++ cards.rb
@@ -1,28 +1,27 @@
 require_relative 'functions'
-
-prompt = '> '
+require 'tty-prompt'
 
 hand = Hash.new(0)
+id = nil
 
-show_hand(hand)
+loop do
+  prompt = TTY::Prompt.new
+  choices = { 'New Game' => 1, 'Draw a Card' => 2, 'Show Hand' => 3, 'Quit' => 4 }
+  choice = prompt.select('Select Action:', choices)
 
-puts 'Welcome to my card game.'
-puts prompt
-
-id = new_game
-card = draw_card(id)
-hand[card] += 1
-
-card = draw_card(id)
-hand[card] += 1
-
-card = draw_card(id)
-hand[card] += 1
-
-card = draw_card(id)
-hand[card] += 1
-
-card = draw_card(id)
-hand[card] += 1
-
-show_hand(hand)
+  case choice
+  when 1
+    id = new_game
+  when 2
+    if id.nil?
+      puts 'You need to start a new game first'
+    else
+      card = draw_card(id)
+      hand[card] += 1
+    end
+  when 3
+    show_hand(hand)
+  when 4
+    exit
+  end
+end
